cc7069d0d20b57969d3505763d58a6341542679e
HumorZhang <373014368@qq.com>
2022-10-17 16:07:29 +0800

    添加Integer stream

--- src/main/java/com/example/demo/controller/Test.java
+++ src/main/java/com/example/demo/controller/Test.java
@@ -1,7 +1,13 @@
 package com.example.demo.controller;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println(1);
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Stream<Integer> stream = integerList.stream().filter(i -> i > 3);
     }
 }
