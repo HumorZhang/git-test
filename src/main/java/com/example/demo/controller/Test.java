7a79e79416cc8cb04cb1bb1eec7dbdd4b93266a8
HumorsZhang <373014368@qq.com>
2024-07-20 21:19:21 +0800

    fix: add

--- src/main/java/com/example/demo/controller/Test.java
+++ src/main/java/com/example/demo/controller/Test.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 public class Test {
     public static void main(String[] args) {
         System.out.println(1);
+        System.out.println(2);
         List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6);
         Stream<Integer> stream = integerList.stream().filter(i -> i > 3);
     }
